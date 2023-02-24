03d806a6c12ffb43319d460302add6c99e5045bd
Bootmaca <clement.hadj@outlook.fr>
2023-02-24 18:21:32 +0100

    Version 1.0.6 : Ajout des imports

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,6 @@
-
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.functions._
 object Main {
   def main(args: Array[String]): Unit = {
 
