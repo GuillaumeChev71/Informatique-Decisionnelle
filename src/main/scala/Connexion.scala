c2ba2aa1bad0dcdda7e687d86149cb0fe5f1f32a
Bootmaca <clement.hadj@outlook.fr>
2023-02-26 13:57:34 +0100

    Version 1.0.6 : Ajout des imports

--- src/main/scala/Connexion.scala
+++ src/main/scala/Connexion.scala
@@ -4,19 +4,24 @@ class Connexion(private var typeConnexion:String) {
   // Class variables
   var connectionProperties: Properties = new Properties()
   var url: String = ""
-  var username: String = "ch098407"
-  var password: String = "ch098407"
+  var username: String = ""
+  var password: String = ""
 
-  //Initialisation des variables
-  connectionProperties.setProperty("user", username)
-  connectionProperties.setProperty("password", password)
 
   if(typeConnexion.equals("Oracle")){
     url = "jdbc:oracle:thin:@stendhal.iem:1521:ENSS2022"
     connectionProperties.setProperty("driver", "oracle.jdbc.driver.OracleDriver")
+    username = "ch098407"
+    password = "ch098407"
   }else{
     url = "jdbc:postgresql://stendhal:5432/tpid2020"
     connectionProperties.setProperty("driver", "org.postgresql.Driver")
+    username = "tpid"
+    password = "tpid"
   }
+
+  //Initialisation des variables
+  connectionProperties.setProperty("user", username)
+  connectionProperties.setProperty("password", password)
 
 }
