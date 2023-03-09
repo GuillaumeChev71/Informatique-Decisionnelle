af4c66d4d064ecbed1e7f113763359aaa7857ca6
Bootmaca <clement.hadj@outlook.fr>
2023-03-09 20:57:11 +0100

    Version 1.1.1 : Modif début des requêtes

--- README.md
+++ README.md
@@ -1,41 +1,79 @@
 Data warehouse :
 
 - Data mart : Commerce
-    - Analyse secteur activite (horaire d'ouverture)
-    - nb commentaire
-    - moyenne etoile
-    - nbClient
-    - Tout ça part jour par semaine par mois
-    - Par geographie aussi
-    - notation pas rapport au menu
-    - Notation si ils ont un parking
+  - Analyse secteur activite (horaire d'ouverture)
+  - nb commentaire
+  - moyenne etoile
+  - nbClient
+  - Tout ça part jour par semaine par mois
+  - Par geographie aussi
+  - notation pas rapport au menu
+  - Notation si ils ont un parking
+  - Nombre de visiteur
 
-- Data mart : Utilisateur
-    - Utilisateur qui visite souvent
-    - Utilisateur qui commentent souvent
-    - Utilisateurs qui ont le plus de réaction à leurs commentaire
-    - nombre d'amis
-    - Le nombre d'arrivé des utilisateurs par années par mois, jours etc
-    - Pourcentage entre des commentaires drole ou cool
-    - Type de compliment le plus reçu
-    - Nombre de fan
+- Data mart : Utilisateurs au sein de l'applis
+  - Utilisateur qui visite souvent
+  - Utilisateur qui commentent souvent
+  - Utilisateurs qui ont le plus de réaction à leurs commentaire
+  - nombre d'amis
+  - Le nombre d'arrivé des utilisateurs par années par mois, jours etc
+  - Pourcentage entre des commentaires drole ou cool
+  - Type de compliment le plus reçu
+  - Nombre de fan
 
-Variable d'environnement du projet : 
+Variable d'environnement du projet :
 ```shell
-export JAVA HOME=/usr/gide/jdk-1.8
-export PATH=usr/gide/jdk-1.8/bin:$PATH
+export JAVA_HOME=/usr/gide/jdk-1.8.0_261
+export PATH=usr/gide/jdk-1.8.0_261/bin:$PATH
 
 export PATH=/usr/gide/sbt-1.3.13/bin:$PATH
 ```
 
-Compilation du projet : 
+
+Compilation du projet :
 ```shell
-sbt clean compile
+    sbt clean compile
 ```
 
-Execution du projet : 
+Execution du projet :
 ```shell
 sbt run
+
+sbt run -J-Xmx4G
 ```
 
 
+Pour Metabase :
+
+Voir les versions
+```shell
+ls /usr/gide
+```
+
+Aller dans le bashrc pour changer les variables d'environnement :
+```shell
+gedit ~/.bashrc
+```
+
+Taper i
+Rajouter les lignes suivantes à la fin :
+```shell
+export PATH=/usr/gide/jdk-12.0.2/bin:$PATH
+export JAVA_HOME=/usr/gide/jdk-12.0.2
+```
+
+La ou est placé le jar créer un dossier "plugins" et y mettre le jar ojdbc
+
+Lancer Metabase:
+```shell
+java -jar metabase.jar
+```
+
+Aller à l'adresse suivante :
+http://localhost:3000/setup
+
+
+mdp metabase :
+```
+ClementGuillaumeMathias3
+```
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,22 +1,78 @@
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql.functions._
+
+
+import org.apache.spark.sql.jdbc.{JdbcDialect, JdbcDialects, JdbcType}
+import org.apache.spark.sql.types._
+
+
 object Main {
   def main(args: Array[String]): Unit = {
 
-    val conn: Connexion = new Connexion("Postgres")
-    println(conn.url)
+    val connPostgres: Connexion = new Connexion("Postgres")
+    val connOracle: Connexion = new Connexion("Oracle")
+
+    //Appel du dialect
+    val dialect = new OracleDialect
+    JdbcDialects.registerDialect(dialect)
 
 
     //Initialisation de Spark
     val spark = SparkSession.builder.appName("ETL").master("local[4]").getOrCreate()
 
-    spark.read
+
+    /**************** CSV ****************/
+
+    val fichierCSV = "yelp_academic_dataset_tip.csv"
+
+    //Chargement du fichier CSV
+    var tips = spark.read.format("csv").option("header", "true").load(fichierCSV)
+
+    //Extraction des user_id
+    var tip = tips.select("business_id", "compliment_count", "date", "user_id")
+
+    //Insertion des données
+    tip.write.mode(SaveMode.Overwrite).jdbc(connOracle.url, "tip", connOracle.connectionProperties)
+
+    /*******************************************/
+
+
+
+
+
+    /**************** POSTGRES ****************/
+    val users = spark.read.jdbc(connPostgres.url, "yelp.user", connPostgres.connectionProperties)
+
+    val dimensionUtilisateur = users.select("user_id", "name")
+
+    //Insertion des données
+    dimensionUtilisateur.write.mode(SaveMode.Overwrite).jdbc(connOracle.url, "utilisateur", connOracle.connectionProperties)
+
+
+
+    val reviews = spark.read.jdbc(connPostgres.url, "yelp.review", connPostgres.connectionProperties)
+
+    val review = users.select("user_id", "name")
+
+    //Insertion des données
+    user.write.mode(SaveMode.Overwrite).jdbc(connOracle.url, "utilisateur", connOracle.connectionProperties)
+
+    //val donnees = df.select(col("date")).show()
+
+
+    /*spark.read
       .option("partitionColumn", "yelping_since")
       .option("lowerBound", "2004-10-12")
       .option("upperBound", "2019-12-13")
       .option("numPartitions", 50)
-      .jdbc(conn.url, "yelp.\"user\"", conn.connectionProperties)
+      .jdbc(conn.url, "yelp.\"user\"", conn.connectionProperties)*/
+
+    /*******************************************/
+
+
+
+
 
     /*
     //Initialisation de Spark
@@ -37,5 +93,10 @@ object Main {
 
     // Enregistrement du DataFrame users dans la table "business"
     business.write.mode(SaveMode.Overwrite).jdbc(conn.retournerUrl(), "business", conn.seConnecter())*/
+
+
+    spark.stop()
   }
 }
+
+
